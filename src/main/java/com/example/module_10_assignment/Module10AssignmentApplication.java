b2174cae9b085e794d0eab44f65d157c02f06f7a
MD. Raisul Islam Niloy <raisulislamniloy47@gmail.com>
2026-02-11 19:32:19 +0600

    first commit

---
+++ src/main/java/com/example/module_10_assignment/Module10AssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.ostad_java_springboot_3_module_10_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OstadJavaSpringBoot3Module10AssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OstadJavaSpringBoot3Module10AssignmentApplication.class, args);
+    }
+
+}
